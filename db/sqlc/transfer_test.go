83b5bdc690164a460f81487b86e518aa0db18c8d
Cao Tri Ngoc <tringoc2077@gmail.com>
2025-07-05 13:22:28 +0700

    unit test entry, transfer

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,62 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/TriNgoc2077/Simple-Bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: util.RandomInt(1, 5),
+		ToAccountID: util.RandomInt(1, 5),
+		Amount: util.RandomInt(1, 100),
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+	return transfer
+}
+
+func TestCreateTransfer(t *testing.T) {
+	createRandomTransfer(t);
+}
+
+func TestGetTransfer(t *testing.T) {
+	transfer1 := createRandomTransfer(t)
+	transfer2, err := testQueries.GetTransfer(context.Background(), transfer1.ID)
+	require.NoError(t, err);
+	require.NotEmpty(t, transfer2)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.Equal(t, transfer1.ToAccountID, transfer2.ToAccountID)
+	require.Equal(t, transfer1.Amount, transfer2.Amount)
+	require.Equal(t, transfer1.FromAccountID, transfer2.FromAccountID)
+	require.WithinDuration(t, transfer1.CreatedAt, transfer2.CreatedAt, time.Second)
+}
+
+func TestListTransfer(t *testing.T) {
+	for i:=0; i<10; i++ {
+		createRandomTransfer(t)
+	}
+
+	arg := ListTransferParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	transfers, err := testQueries.ListTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, transfers, 5)
+
+	for _, transfer := range transfers {
+		require.NotEmpty(t, transfer)
+	}
+}
